907eb07162fcc553729b528c03f7377a6f78cf6a
Felipe Lustosa <lustosaf590@gmail.com>
2024-11-15 15:08:18 -0300

    final

---
+++ src/main/java/com/devsuperior/aula/dto/PersonDTO.java
@@ -0,0 +1,50 @@
+package com.devsuperior.aula.dto;
+
+import com.devsuperior.aula.entities.Department;
+
+public class PersonDTO {
+    private Long id;
+    private String name;
+    private Double salary;
+
+    private Department department;
+
+    public PersonDTO(Long id, String name, Double salary, Department department) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+        this.department = department;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+}
